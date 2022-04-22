8f3e2cbc704d14f1791c5a742ae270f4a8d071f5
unknown <adrimart212@gmail.com>
2022-04-22 22:56:13 +0200

    Hecho con animación y gif

--- Ejercicio bombilla/main.js
+++ Ejercicio bombilla/main.js
@@ -1,12 +1,17 @@
 const btn = document.getElementById("interruptor");
 var help=0;
 btn.onclick = () => {
+    document.getElementById("body").classList.toggle("bodyy");
     if (help == 0){
         btn.src = "bon.jpg";
         help++;
+        startConfetti();
+        document.getElementById("containerId").style.display = "block";
     }else{
         btn.src = "boff.jpg";
         help--;
+        stopConfetti();
+        document.getElementById("containerId").style.display = "none";
     }
         
     }
